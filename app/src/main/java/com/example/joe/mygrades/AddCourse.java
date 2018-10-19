6060f7470390dd3b09951af99abae242de55b8f7
Joseph Lyons <jlyons014@gmail.com>
2018-10-18 23:56:34 -0400

    Designed menu_add_course layout and content_add_course layout. Implemented some code in AddCourse class.

---
+++ app/src/main/java/com/example/joe/mygrades/AddCourse.java
@@ -0,0 +1,48 @@
+package com.example.joe.mygrades;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public class AddCourse extends AppCompatActivity {
+
+    Intent intent;
+
+    Bundle bundle;
+
+    long id;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        bundle = this.getIntent().getExtras();
+
+        //get the ID in the bundle
+        id = bundle.getLong("_id");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem course){
+        return true;
+    }
+
+
+}
